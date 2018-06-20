64b87597a1bbb16f8897b71fe2a892508ef09ea9
Carlos Guerrero <carlos@MacBook-Pro-de-Carlos.local>
2018-06-20 14:03:27 +0200

    Article accepted in Journal AIHC

--- src/org/fog/test/perfeval/NApps.java
+++ src/org/fog/test/perfeval/NApps.java
@@ -1,3 +1,32 @@
+/*
+* Copyright 2018 Carlos Guerrero, Isaac Lera.
+* 
+* Created on Nov 09 08:10:55 2018
+* @authors:
+*     Carlos Guerrero
+*     carlos ( dot ) guerrero  uib ( dot ) es
+*     Isaac Lera
+*     isaac ( dot ) lera  uib ( dot ) es
+* 
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+* 
+* 
+* This extension has been implemented for the research presented in the 
+* article "A lightweight decentralized service placement policy for 
+* performance optimization in fog computing", accepted for publication 
+* in "Journal of Ambient Intelligence and Humanized Computing".
+*/
+
 package org.fog.test.perfeval;
 
 import java.util.ArrayList;
